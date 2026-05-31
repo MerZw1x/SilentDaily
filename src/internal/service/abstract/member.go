ce825712f5521b2303e058ff84eabf71b4ded1be
Malov Tim <t.malov2014@gmail.com>
2026-06-01 00:01:33 +0300

    make member service

--- src/internal/service/abstract/member.go
+++ src/internal/service/abstract/member.go
@@ -1,4 +1,11 @@
 package abstract
 
-// IMemberService — контракт сервиса управления участниками команды
-type IMemberService interface{}
+import (
+	"silent/src/internal/db/abstract"
+	"silent/src/internal/domain"
+)
+
+type IMemberService interface {
+	Register(conn abstract.IDBConnection, telegramUserID int, name string, teamID int, isLead bool) error
+	GetByTelegramID(conn abstract.IDBConnection, telegramUserID int) (*domain.Member, error)
+}
--- src/internal/service/impl/member.go
+++ src/internal/service/impl/member.go
@@ -1,4 +1,56 @@
 package impl
 
-// MemberService — реализация IMemberService
-type MemberService struct{}
+import (
+	"errors"
+	aconn "silent/src/internal/db/abstract"
+	"silent/src/internal/domain"
+	"silent/src/internal/repository/abstract"
+	"time"
+)
+
+type MemberService struct {
+	MemberRepository abstract.IMemberRepository
+}
+
+func NewMemberRepository(memberRepository abstract.IMemberRepository) *MemberService {
+	return &MemberService{
+		MemberRepository: memberRepository,
+	}
+}
+
+func (service *MemberService) Register(conn aconn.IDBConnection, telegramUserID int, name string, teamID int, isLead bool) error {
+	member, err := service.MemberRepository.GetByTelegramID(conn, telegramUserID)
+	if err != nil {
+		return err
+	}
+	if member != nil {
+		return errors.New("the user already exists")
+	}
+
+	member = &domain.Member{
+		TeamID:         teamID,
+		TelegramUserID: telegramUserID,
+		Name:           name,
+		IsLead:         isLead,
+		CreatedAt:      time.Now(),
+	}
+
+	err = service.MemberRepository.Create(conn, member)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (service *MemberService) GetByTelegramID(conn aconn.IDBConnection, telegramUserID int) (*domain.Member, error) {
+	member, err := service.MemberRepository.GetByTelegramID(conn, telegramUserID)
+	if err != nil {
+		return nil, err
+	}
+	if member == nil {
+		return nil, errors.New("the user does not exist")
+	}
+
+	return member, nil
+}
